25a95bc7d5ba000402a1fb2800f4e3d3521011ad
Karan <kairakaran362@gmail.com>
2021-07-01 02:50:15 +0530

    changed about

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return (
     <div>
         <header>
-            <h1> Keeper - Clone of Google Keep with CRUD capabilities</h1>
+            <h1> Note It - Clone of Google Keep with CRUD capabilities</h1>
         </header>
     </div>
     );
--- src/components/notes/AboutPage.jsx
+++ src/components/notes/AboutPage.jsx
@@ -6,7 +6,7 @@ import React from "react";
 function AboutPage() {
     return (
         <div className="about"> 
-            <h1>"Keeper" is a simple note-taking app built with React.js, Twitter Bootstrap, Node.js, Express, and MongoDB.</h1>
+            <h1>"Note It" is a simple note-taking app built with React.js, Twitter Bootstrap, Node.js, Express, and MongoDB.</h1>
                 <p> The application allows you to browse through a list of notes, as well as add, update, and delete notes.</p>
                 
             
